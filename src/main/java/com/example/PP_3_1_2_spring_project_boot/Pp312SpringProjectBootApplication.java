5c05599e296ec7d94781a1eb88ca92d761ef6a62
Sleepforadream <maxnervannn@gmail.com>
2023-05-11 16:02:24 +0500

    update 11.05.2023

---
+++ src/main/java/com/example/PP_3_1_2_spring_project_boot/Pp312SpringProjectBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.PP_3_1_2_spring_project_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pp312SpringProjectBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pp312SpringProjectBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PP_3_1_2_spring_project_boot/Pp312SpringProgectBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PP_3_1_2_spring_project_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pp312SpringProgectBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
